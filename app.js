83c5a3d638408ac615821fe7ca9cbc0e44d0550c
mastro150670 <nazaretj@hotmail.com>
2023-07-14 19:08:15 -0300

    proyecto finalizado hoy 15-julio

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
   window.addEventListener("load", function() {
     document.getElementById("Alerta").addEventListener("click" , function() {
         alert("formulario correcto");
+        document.write("Mensaje enviado")
     })
 })
